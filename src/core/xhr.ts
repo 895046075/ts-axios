ad2fcc5c4a6c0fac7d950d896328fae34ee826ba
WuJie <895046075@qq.com>
2021-06-27 23:33:51 +0800

    🎈 perf(core): 把核心代码放入core文件夹

--- src/core/xhr.ts
+++ src/core/xhr.ts
@@ -1,6 +1,7 @@
-import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from "./types";
-import { parseHeaders } from "./helpers/headers";
-import { createError } from "./helpers/error";
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from '../types';
+import { parseHeaders } from '../helpers/headers';
+import { createError } from '../helpers/error';
+
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve,reject) => {
     const { data = null, url, method = 'get',headers, responseType,timeout } = config
@@ -11,7 +12,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
     if (timeout) {
       request.timeout = timeout
     }
-    request.open(method.toUpperCase(), url, true)
+    request.open(method.toUpperCase(), url!, true)
     request.onreadystatechange = function handleLoad() {
       if (request.readyState !== 4) {
         return
